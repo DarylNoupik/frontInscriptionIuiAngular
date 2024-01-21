6795627439e973196a907f8a6e85e404f756ab19
kepya <christiankepya@gmail.com>
2024-01-21 11:12:25 +0100

    update

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -7,6 +7,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  matters: {title: string, formation?: string, content: {title: string, duration: string, coef: number}[] }[] = [
+    {
+    title: '1er Cycle',
+    content: [
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Physique - Chimie',
+        duration: '2H',
+        coef: 3
+      },
+      {
+        title: 'Anglais - IT',
+        duration: '2H',
+        coef: 4
+      },
+    ]
+  },
+    {
+    title: '2ème Cycle',
+    formation: 'X',
+    content: [
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+    ]
+  },
+    {
+    title: '2ème Cycle',
+    formation: 'I',
+    content: [
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+    ]
+  },
+    {
+    title: '2ème Cycle',
+    formation: 'IP',
+    content: [
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+      {
+        title: 'Matématiques',
+        duration: '2H',
+        coef: 4
+      },
+    ]
+  }
+];
+  
   constructor() { }
 
   ngOnInit(): void {
