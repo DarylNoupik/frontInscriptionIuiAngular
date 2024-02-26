1f1d88ee091f89e7c953e742ea6022226bc67e48
Mohammadou Sadjo <mohammadou.sadjo@2023.ucac-icam.com>
2024-02-26 01:59:31 +0100

    Added some updates sent by Daryl

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -3,104 +3,120 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  matters: {
+    title: string;
+    formation?: string;
+    content: { title: string; duration: string; coef: number }[];
+  }[] = [
+    {
+      title: '1er Cycle',
+      content: [
+        {
+          title: 'Mathématiques',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Physique - Chimie',
+          duration: '2H',
+          coef: 3,
+        },
+        {
+          title: 'Anglais - IT',
+          duration: '2H',
+          coef: 4,
+        },
+      ],
+    },
+    {
+      title: '2ème Cycle',
+      formation: 'X',
+      content: [
+        {
+          title: 'Mathématiques',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Anglais ',
+          duration: '1H',
+          coef: 1.5,
+        },
+        {
+          title: 'Informatique',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Culture générale',
+          duration: '1H',
+          coef: 2,
+        },
+      ],
+    },
+    {
+      title: '2ème Cycle',
+      formation: 'I',
+      content: [
+        {
+          title: 'Mathématiques',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Anglais',
+          duration: '1H',
+          coef: 1.5,
+        },
+        {
+          title: ' Electrotechnique / Thermodynamique ',
+          duration: '2H',
+          coef: 3,
+        },
+        {
+          title: 'Culture générale ',
+          duration: '1H',
+          coef: 2,
+        },
+      ],
+    },
+    {
+      title: '2ème Cycle',
+      formation: 'IP',
+      content: [
+        {
+          title: 'Mathématiques',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Anglais ',
+          duration: '1H',
+          coef: 1.5,
+        },
+        {
+          title: 'Chimie',
+          duration: '2H',
+          coef: 4,
+        },
+        {
+          title: 'Culture générale',
+          duration: '1H',
+          coef: 2,
+        },
+      ],
+    },
+  ];
 
-  matters: {title: string, formation?: string, content: {title: string, duration: string, coef: number}[] }[] = [
-    {
-    title: '1er Cycle',
-    content: [
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Physique - Chimie',
-        duration: '2H',
-        coef: 3
-      },
-      {
-        title: 'Anglais - IT',
-        duration: '2H',
-        coef: 4
-      },
-    ]
-  },
-    {
-    title: '2ème Cycle',
-    formation: 'X',
-    content: [
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-    ]
-  },
-    {
-    title: '2ème Cycle',
-    formation: 'I',
-    content: [
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-    ]
-  },
-    {
-    title: '2ème Cycle',
-    formation: 'IP',
-    content: [
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-      {
-        title: 'Matématiques',
-        duration: '2H',
-        coef: 4
-      },
-    ]
-  }
-];
-  
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goTopPage(): void {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-
 }
--- src/app/shared/validators/number_validator.ts
+++ src/app/shared/validators/number_validator.ts
@@ -1,121 +1,121 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function createCamerounianNumberValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
 
-        if (!value) {
-            return null;
-        }
-
-        const regExp = new RegExp(/^6(5|7|8|9)[0-9]{7}$/);
-
-        if (regExp.test(value)) {
-            return null;
-        } else {
-            return {
-                notConform: true
-            };
-        }
+    if (!value) {
+      return null;
     }
+
+    const regExp = new RegExp(/^6(5|7|8|9)[0-9]{7}$/);
+    const regExpAll = new RegExp(/^[0-9]{9}$/);
+
+    if (regExp.test(value) || regExpAll.test(value)) {
+      return null;
+    } else {
+      return {
+        notConform: true,
+      };
+    }
+  };
 }
 
 export function createInternationalNumberValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
 
-        if (!value) {
-            return null;
-        }
-
-        const phoneInterNumberRegex = /^(\+[1-9]{1}[0-9]{3,14})?([0-9]{9,14})$/;
-
-        if (phoneInterNumberRegex.test(value) ) {
-            return null;
-        } else {
-            return {
-                notConform: true
-            };
-        }
+    if (!value) {
+      return null;
     }
+
+    const phoneInterNumberRegex = /^(\+[1-9]{1}[0-9]{3,14})?([0-9]{9,14})$/;
+
+    if (phoneInterNumberRegex.test(value)) {
+      return null;
+    } else {
+      return {
+        notConform: true,
+      };
+    }
+  };
 }
 
 export function createStringValidatior(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
 
-        if (!value) {
-            return null;
-        }
-
-        let spaceCheck = /^ *$/;
-        if (spaceCheck.test(value)) {
-            return {
-                notConform: true
-            };
-        }
-
-        let numberCheck = /^\d+$/;
-        if (numberCheck.test(value)) {
-            return {
-                notConform: true
-            };
-        }
-
-        return null;
+    if (!value) {
+      return null;
     }
+
+    let spaceCheck = /^ *$/;
+    if (spaceCheck.test(value)) {
+      return {
+        notConform: true,
+      };
+    }
+
+    let numberCheck = /^\d+$/;
+    if (numberCheck.test(value)) {
+      return {
+        notConform: true,
+      };
+    }
+
+    return null;
+  };
 }
 
 export function emailValidatior(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
 
-        if (!value) {
-            return null;
-        }
-
-        let emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if (!emailCheck.test(value)) {
-            return {
-                notConform: true
-            };
-        }
-
-        return null;
+    if (!value) {
+      return null;
     }
+
+    let emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailCheck.test(value)) {
+      return {
+        notConform: true,
+      };
+    }
+
+    return null;
+  };
 }
 
-
 export function dateValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
 
-        if (!value) {
-            return null;
-        }
-   
-        let date = new Date(value);
-
-        let dateCurrent = new Date();
-
-        let dateBefore15 = new Date();
-        dateBefore15.setFullYear(dateCurrent.getFullYear() - 15);
-
-        let dateBefore30 = new Date();
-        dateBefore30.setFullYear(dateCurrent.getFullYear() - 30);
-
-        if (date.getTime() > dateBefore15.getTime()) {
-            return {
-                max: true
-            };
-        }
-
-        if (date.getTime() < dateBefore30.getTime()) {
-            return {
-                notConform: true
-            };
-        }
-
-return null;
+    if (!value) {
+      return null;
     }
+
+    let date = new Date(value);
+
+    let dateCurrent = new Date();
+
+    let dateBefore15 = new Date();
+    dateBefore15.setFullYear(dateCurrent.getFullYear() - 15);
+
+    let dateBefore30 = new Date();
+    dateBefore30.setFullYear(dateCurrent.getFullYear() - 30);
+
+    if (date.getTime() > dateBefore15.getTime()) {
+      return {
+        max: true,
+      };
+    }
+
+    if (date.getTime() < dateBefore30.getTime()) {
+      return {
+        notConform: true,
+      };
+    }
+
+    return null;
+  };
 }
